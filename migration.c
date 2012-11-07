e707596e1a446e553ae51dc1f40d6742ad4209b5
Paolo Bonzini <pbonzini@redhat.com>
2012-11-08 00:42:50 +0100

    migration: fix qemu_get_fd for BufferedFile
    Not really used, but nice to have it correct. :)
    
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

--- migration.c
+++ migration.c
@@ -543,7 +543,7 @@ static int buffered_get_fd(void *opaque)
 {
     QEMUFileBuffered *s = opaque;
 
-    return qemu_get_fd(s->file);
+    return s->migration_state->fd;
 }
 
 static int buffered_rate_limit(void *opaque)
